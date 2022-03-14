b4655e705234ae6e60c3a7206b338d95def5cf45
Judit Ferenczik <jferenczik@gmail.com>
2022-03-14 13:34:23 +0100

    add full year calendar events

--- app/src/main/java/com/example/beosztasapp/activities/CalendarActivity.kt
+++ app/src/main/java/com/example/beosztasapp/activities/CalendarActivity.kt
@@ -7,20 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.applandeo.materialcalendarview.CalendarDay
 import kotlinx.android.synthetic.main.activity_calendar.*
 import java.util.*
-import com.applandeo.materialcalendarview.EventDay
-import com.example.beosztasapp.R
 import kotlin.collections.ArrayList
+import android.widget.ArrayAdapter
+import android.widget.CalendarView
+import android.widget.Spinner
+import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
+import com.example.beosztasapp.R
 
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.beosztasapp.R.layout.activity_calendar)
-      //  val calendarView: CalendarView = findViewById<View>(R.id.calendarView) as CalendarView
+      //val calendarView: CalendarView = findViewById<View>(R.id.calendarView) as CalendarView
 
         val min: Calendar = Calendar.getInstance()
         val max: Calendar = Calendar.getInstance()
-        min.set(2022,1,1)
+        min.set(2021,12,1)
         max.set(2022,12,31)
 
         calendarView.setMinimumDate(min)
@@ -42,80 +46,136 @@ class CalendarActivity : AppCompatActivity() {
 
             }
         )
+        val dropdown = findViewById<Spinner>(com.example.beosztasapp.R.id.spinner1)
+        val items = arrayOf("Táppénz", "Szabadság")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        dropdown.adapter = adapter
      //   val calendars: ArrayList<Calendar> = ArrayList()
-        for (i in 0..daysInMonth( Calendar.DAY_OF_MONTH, Calendar.YEAR)-1) {
+        val calendar1 = Calendar.getInstance()
+        var monthCount:Int = 1
+        var currentdaysMonth:Int = 0
+        for (j in 0..calendar1.getActualMaximum(Calendar.DAY_OF_YEAR)){
             val calendar2 = Calendar.getInstance()
-            calendar2[Calendar.DAY_OF_MONTH] = 1
-            calendar2.add(Calendar.DAY_OF_MONTH, i)
-            val tmpDate: String = "2022." + (Calendar.MONTH + 1).toString() + "." + (i+1).toString()
+            calendar2[Calendar.DAY_OF_YEAR] = 1
+            calendar2.add(Calendar.DAY_OF_YEAR, j)
+            currentdaysMonth++
+            if(daysInMonth(monthCount, 2022) < currentdaysMonth){
 
-            Toast.makeText(
-                this@CalendarActivity,
-                daysInMonth(Calendar.DAY_OF_MONTH, Calendar.YEAR).toString(),
-                Toast.LENGTH_SHORT
-            ).show()
-            Toast.makeText(this@CalendarActivity,isAthelyezett(tmpDate).toString(),Toast.LENGTH_SHORT).show()
-            when {
-                isWeekend(tmpDate) ->{
-                    events.add(
-                        EventDay(
-                            calendar2,
-                            R.drawable.logohetvege,
-                            Color.parseColor("#9C5700")
-                        )
-                    )
-                }
-
-                isAthelyezett(tmpDate) -> {
-                    events.add(
-                        EventDay(
-                            calendar2,
-                            R.drawable.logowork,
-                            Color.parseColor("#9C5700")
-                        )
-                    )
-                }
-                isMunkaszunet(tmpDate) -> {
-                    events.add(
-                        EventDay(
-                            calendar2,
-                            R.drawable.logopiheno,
-                            Color.parseColor("#FFC7CE")
-                        )
-                    )
-                }
-                isPiheno(tmpDate) -> {
-                    events.add(
-                        EventDay(
-                            calendar2,
-                            R.drawable.logohetvege,
-                            Color.parseColor("#FFC7CE")
-                        )
-                    )
-                }
-                else -> {  //if(isWorkday(tmpDate))
-                    events.add(
-                        EventDay(
-                            calendar2,
-                            R.drawable.logowork,
-                            Color.parseColor("#006100")
-                        )
-                    )
-                }
+               // currentdaysMonth -= daysInMonth(monthCount, 2022)
+                Toast.makeText(this,daysInMonth(monthCount, 2022).toString()+",month:"+monthCount.toString()+ "days:" + currentdaysMonth,Toast.LENGTH_LONG ).show()
+                monthCount++
+                currentdaysMonth -= daysInMonth(monthCount, 2022)
             }
+         //   for (i in 0..daysInMonth( Calendar.DAY_OF_MONTH, Calendar.YEAR)-1) {
+               // val calendar2 = Calendar.getInstance()
+             //   calendar2[Calendar.DAY_OF_MONTH] = 1
+             //   calendar2.add(Calendar.DAY_OF_MONTH, i)
+                val tmpDate: String = "2022." + monthCount.toString() + "." + currentdaysMonth.toString()
+           //     val tmpDate: String = "2022." + (j+1).toString() + "." + (i+1).toString()
 
+
+                /*  Toast.makeText(
+                      this@CalendarActivity,
+                      daysInMonth(Calendar.DAY_OF_MONTH, Calendar.YEAR).toString(),
+                      Toast.LENGTH_SHORT
+                  ).show()*/
+                // Toast.makeText(this@CalendarActivity,tmpDate,Toast.LENGTH_SHORT).show()
+                //calendar2.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+                when {
+                    isAthelyezett(tmpDate) ->{
+                      //  Toast.makeText(this,tmpDate,Toast.LENGTH_LONG ).show()
+                        events.add(
+                            EventDay(
+                                calendar2,
+                                com.example.beosztasapp.R.drawable.logowork,
+                                Color.parseColor("#9C5700")
+                            )
+                        )
+                    }
+                    isMunkaszunet(tmpDate) -> {
+                    //    Toast.makeText(this,tmpDate,Toast.LENGTH_LONG ).show()
+                        events.add(
+                            EventDay(
+                                calendar2,
+                                com.example.beosztasapp.R.drawable.logopiheno,
+                                Color.parseColor("#FFC7CE")
+                            )
+                        )
+                    }
+                    isPiheno(tmpDate) -> {
+                      //  Toast.makeText(this,tmpDate,Toast.LENGTH_LONG ).show()
+                        events.add(
+                            EventDay(
+                                calendar2,
+                                com.example.beosztasapp.R.drawable.logohetvege,
+                                Color.parseColor("#FFC7CE")
+                            )
+                        )
+                    }
+                    calendar2.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || calendar2.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ->{
+                        events.add(
+                            EventDay(
+                                calendar2,
+                                com.example.beosztasapp.R.drawable.logohetvege,
+                                Color.parseColor("#9C5700")
+                            )
+                        )
+                    }
+
+
+
+                    else -> {  //if(isWorkday(tmpDate))
+                        events.add(
+                            EventDay(
+                                calendar2,
+                                com.example.beosztasapp.R.drawable.logowork,
+                                Color.parseColor("#006100")
+                            )
+                        )
+                    }
+               // }
+
+            }
         }
+
         // calendars.add(Calendar.DAY_OF_MONTH,1)
         calendarView.setEvents(events)
 
         //    calendarView.setCalendarDays(getSelectedDays())
 
-        calendarView.setCalendarDays(listP)
+        calendarView.setOnDayClickListener(object : OnDayClickListener {
+            override fun onDayClick(eventDay: EventDay) {
+                val calendar = Calendar.getInstance()
+
+                var year = 2021 + Calendar.YEAR
+                var month = Calendar.MONTH +1
+                var day = Calendar.DAY_OF_MONTH
+                var allday = Calendar.DAY_OF_YEAR
+                calendar.clear()
+                var selectedOption:String =  spinner1.getSelectedItem().toString()
+                var clickedDayCalendar = selectedOption+ allday.toString() + year.toString() +"."+month.toString()+"."+day.toString()
+                et_picked_date.setText(clickedDayCalendar)
+              //  calendarView.getSelectedDates()
+
+                Toast.makeText(this@CalendarActivity,clickedDayCalendar,Toast.LENGTH_SHORT).show()
+            }
+        })
+        /*
+
+        val calendar = findViewById<CalendarView>(R.id.calendarView)
+        calendar.setOnDateChangeListener(CalendarView.OnDateChangeListener { _, i, il, i2 ->
+            var date = "$i/$il/$i2"
+            et_picked_date.setText(date)
+            Toast.makeText(this@CalendarActivity,date,Toast.LENGTH_SHORT).show()
+
+        })*/
+
+
     }
     open fun isWeekend(myDate:String):Boolean {
         val listWeekend = listOf("2022.3.5","2022.3.6","2022.3.12","2022.3.13","2022.3.19","2022.3.20","2022.3.26","2022.3.27","2022.10.26")
-        Toast.makeText(this@CalendarActivity, myDate,Toast.LENGTH_LONG).show()
-        Toast.makeText(this@CalendarActivity, listWeekend.contains(myDate).toString(),Toast.LENGTH_LONG).show()
+      //  Toast.makeText(this@CalendarActivity, myDate,Toast.LENGTH_LONG).show()
+     //   Toast.makeText(this@CalendarActivity, listWeekend.contains(myDate).toString(),Toast.LENGTH_LONG).show()
 
         return listWeekend.contains(myDate)  && !isAthelyezett(myDate)
 
